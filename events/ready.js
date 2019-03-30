c91fb4f2ea1c0ef97ec4e33fa396bb9cd18d66d3
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-03-31 01:18:31 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,20 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`OkeanosGG | "?yardım"`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+   var oyun = [
+        "Eklenicek Yapım Aşamasında1.",
+        "Eklenicek Yapım Aşamasında2.",
+        "💪 7/24 Aktif!",  
+        "OkeanosGG | ?yardım",
+        "Eklenicek Yapım Aşamasında4.",
+        "Eklenicek Yapım Aşamasında5.",
+        "Eklenicek Yapım Aşamasında6."
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/emirhansaracyt");
+        }, 2 * 2500);
+}
