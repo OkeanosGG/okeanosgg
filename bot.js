081e6c63eb145070b7db66255b2e1ab522d4b460
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-03-31 01:44:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,6 +115,30 @@ if (msg.content.toLowerCase() === prefix + 'benimleevlenirmisineren' ) { msg.cha
 
 });
 
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "davet-takip");
+    logChannel.send(`**${member.user.tag}** Sunucuya kat?ld? davet eden **${inviter.tag}** daveti kullanan kisi say?s? **${invite.uses}**`);
+  });
+});
+
 client.on('message', async message => {
   const ms = require('ms');
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
