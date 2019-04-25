d4d6b28d4d453b9a600ef8f8ce97615e53202cae
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-04-25 21:28:04 +0300

    Update herkese-rol-ver.js

--- komutlar/herkese-rol-ver.js
+++ komutlar/herkese-rol-ver.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   var bot = "512292345140609024"
-   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(':hayir: | Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!')
+   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(':hayir: | Bu komutu kullanabilmek için **Yönetici** yetkisine sahip olmalısın!')
    let rol = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(rol => rol.name === args[0]);
   if (!rol) return message.channel.send('Herkese rol verebilmem için bir rol etiketlemelisin.')
   
