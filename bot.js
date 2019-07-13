0b5e9fc36fd70ac4e5cbac1a118b610742ff8910
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-07-13 13:03:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,6 +84,18 @@ client.unload = command => {
   });
 };
 
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}`
+        }})
+    }
+});
+
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
     msg.channel.sendMessage('Aleyküm Selam Güzel Kardeşim!');
