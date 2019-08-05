2ec0ff2cc99efb82560af547193b21ea4bb41fc2
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-08-05 11:24:41 +0300

    Create yardım2.js

---
+++ komutlar/yardım2.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('» Komut Grupları')
+.setTimestamp()
+.addField('» Eğlence Komutları', '?eğlence')
+.addField('» Moderatör Komutları', '?moderatör')
+.addField('» Genel Komutlar', '?genel')
+.addField('» Ekstra Komutlar', '?ekstra')
+.addField('» Nsfw Komutları', '?nsfw')
+.setFooter('© 2019 Okeanos', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
